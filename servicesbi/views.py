5eeb64202548b39e553e9894ca9ec56d71a7f118
Jrleles <jrleles@gmail.com>
2026-01-04 14:28:21 -0300

    Rotas explícitas por página + base.html + ajustes footer e currículo

--- servicesbi/views.py
+++ servicesbi/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "servicesbi/index.html")
+
+def python(request):
+    return render(request, "servicesbi/python.html")
+
+def powerbi(request):
+    return render(request, "servicesbi/powerbi.html")
+
+def automacoes(request):
+    return render(request, "servicesbi/automacoes.html")
+
+def excel(request):
+    return render(request, "servicesbi/excel.html")
+
+def curriculo(request):
+    return render(request, "servicesbi/curriculo.html")
+
+def contato(request):
+    return render(request, "servicesbi/contato.html")
